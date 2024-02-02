536292fd02a6f4dc8fb8b9efd410390a46741547
TensorThinker <158251685+TensorThinker@users.noreply.github.com>
2024-02-02 18:02:56 +0800

    Delete train-alldataset.sh

--- train-alldataset.sh
+++
@@ -1,3 +0,0 @@
-python train_querymatch.py --config ./config_querymatch/refcoco.yaml --config-file ./configs/coco/instance-segmentation/swin/maskformer2_swin_base_384_bs16_50ep.yaml --eval-only MODEL.WEIGHTS ./model_final.pth
-python train_querymatch.py --config ./config_querymatch/refcoco+.yaml --config-file ./configs/coco/instance-segmentation/swin/maskformer2_swin_base_384_bs16_50ep.yaml --eval-only MODEL.WEIGHTS ./model_final.pth
-python train_querymatch.py --config ./config_querymatch/refcocog.yaml --config-file ./configs/coco/instance-segmentation/swin/maskformer2_swin_base_384_bs16_50ep.yaml --eval-only MODEL.WEIGHTS ./model_final.pth
